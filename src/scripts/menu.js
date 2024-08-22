ff2542bd911dfcdecff2718331748c6584211643
exhotik <nomail@nomail.nomail>
2024-08-22 17:33:50 +0200

    feat: fade in animation

---
+++ src/scripts/menu.js
@@ -0,0 +1,18 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            if (entry.target.nodeName === "IMG") {
+                setTimeout(() => { entry.target.style.opacity = 1; }, 100)
+            } else if (entry.target.nodeName === "H1") {
+                setTimeout(() => { entry.target.style.opacity = 1; }, 500)
+            } else if (entry.target.nodeName === "P") {
+                setTimeout(() => { entry.target.style.opacity = 1; }, 800)
+            } else if (entry.target.nodeName === "SECTION") {
+                setTimeout(() => { entry.target.style.opacity = 1; }, 1000)
+            }
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll('[observe-me]');
+hiddenElements.forEach((el) => observer.observe(el))
