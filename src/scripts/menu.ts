088fe2c5315b4277b4ddd73d01ec4fb628c48fdc
exhotik <145331590+exhotik@users.noreply.github.com>
2024-08-25 08:40:56 +0200

    refactor: JS to TS

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -2,13 +2,13 @@ const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
             if (entry.target.nodeName === "IMG") {
-                setTimeout(() => { entry.target.style.opacity = 1; }, 100)
+                setTimeout(() => { (entry.target as HTMLElement).style.opacity = "1"; }, 100)
             } else if (entry.target.nodeName === "H1") {
-                setTimeout(() => { entry.target.style.opacity = 1; }, 500)
+                setTimeout(() => { (entry.target as HTMLElement).style.opacity = "1"; }, 500)
             } else if (entry.target.nodeName === "P") {
-                setTimeout(() => { entry.target.style.opacity = 1; }, 800)
+                setTimeout(() => { (entry.target as HTMLElement).style.opacity = "1"; }, 800)
             } else if (entry.target.nodeName === "SECTION") {
-                setTimeout(() => { entry.target.style.opacity = 1; }, 1000)
+                setTimeout(() => { (entry.target as HTMLElement).style.opacity = "1"; }, 1000)
             }
         }
     })
